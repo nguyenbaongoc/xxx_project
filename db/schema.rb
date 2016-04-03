24f1e1678929c436de1f662e617b0db187da3eb3
khoink94 <khoink94@gmail.com>
2016-04-03 16:05:50 +0000

    added user info

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402155458) do
+ActiveRecord::Schema.define(version: 20160403151143) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,9 +43,12 @@ ActiveRecord::Schema.define(version: 20160402155458) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
