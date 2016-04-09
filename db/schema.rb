56d55ccd72456a72235ce3ff15d0b7dd6360d3ca
khoink94 <khoink94@gmail.com>
2016-04-09 17:52:19 +0000

    changed images attribute comment to caption

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403151143) do
+ActiveRecord::Schema.define(version: 20160409174214) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "user_id"
@@ -19,14 +19,7 @@ ActiveRecord::Schema.define(version: 20160403151143) do
     t.string   "file"
     t.datetime "created_at", null: false
     t.string   "address"
-    t.string   "comment"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "search_suggestions", force: :cascade do |t|
-    t.string   "term"
-    t.integer  "popularity"
-    t.datetime "created_at", null: false
+    t.string   "caption"
     t.datetime "updated_at", null: false
   end
 
