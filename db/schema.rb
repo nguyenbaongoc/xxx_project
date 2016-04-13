3895fa94a502a010f0d893e78a43250cffc9f5ac
Nguyễn Thị Thu Trang <tranghong1994@gmail.com>
2016-04-13 04:27:40 +0000

    restyle index page

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20160411152816) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string   "term"
+    t.integer  "popularity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
